8121b2f6f5c68b5e89e5c3e22401efe99d7108ec
Norbert Deák <thedeaknorbert@gmail.com>
2026-08-11 16:36:13 +0200

    Improve child color generation

--- nodos/world/cities/city.py
+++ nodos/world/cities/city.py
@@ -1,6 +1,9 @@
+import colorsys
 import math
 import random
 from typing import Optional
+
+import numpy as np
 
 from nodos.core.hex_math import HexObject
 
@@ -22,13 +25,7 @@ class City:
         self.name = f'{random.choice(NAME_PREFIXES)}{random.choice(NAME_SUFFIXES)}'
 
         if parent_color:
-            delta = 20
-            self.color = (
-                max(50, min(220, parent_color[0] + random.randint(-delta, delta))),
-                max(50, min(220, parent_color[1] + random.randint(-delta, delta))),
-                max(50, min(220, parent_color[2] + random.randint(-delta, delta))),
-                parent_color[3]
-            )
+            self.color = City._get_new_color(parent_color=parent_color)
         else:
             self.color = (
                 random.randint(a=50, b=220),
@@ -40,3 +37,21 @@ class City:
         self.industrial_angle = random.uniform(a=0.0, b=2.0 * math.pi)
 
         self.districts: dict[HexObject, str] = {center: 'center'}
+
+    @staticmethod
+    def _get_new_color(parent_color: tuple[int, int, int, int]) -> tuple[int, int, int, int]:
+        r, g, b = [x / 255.0 for x in parent_color[:3]]
+        hsv_array = np.array(colorsys.rgb_to_hsv(r=r, g=g, b=b))
+
+        delta_signs = np.random.randint(low=0, high=2, size=3)*2-1
+        diffs = delta_signs * np.random.uniform(low=0.0, high=0.05, size=3)
+
+        h, s, v = tuple(hsv_array + diffs)
+        new_r, new_g, new_b = colorsys.hsv_to_rgb(h=h, s=s, v=v)
+
+        return (
+            int(new_r * 255),
+            int(new_g * 255),
+            int(new_b * 255),
+            parent_color[3]
+        )
